49af1dcc7e5cb048e8750a49448e1445e73fcf7e
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2022-04-05 16:46:25 +0200

    Skip players without a known UUID
    We don't want to respond to players that don't exist, such as fake players that chat through a plugin.

--- src/main/kotlin/me/zeroeightsix/francis/bot/Francis.kt
+++ src/main/kotlin/me/zeroeightsix/francis/bot/Francis.kt
@@ -6,6 +6,9 @@ import me.zeroeightsix.francis.communicate.Emitter
 class Francis : Bot {
 
     override fun respondTo(message: ChatMessage, emitter: Emitter) {
+        // we're not interested in people without UUIDs
+        if (message.sender.isFugitive()) return
+
         if (message.message == "beep") {
             emitter.emitChat(message.reply("boop"))
         }
